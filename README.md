93c8592666057339b93e513d96817cb2438555e7
charnysho <charnysho@gmail.com>
2020-04-09 13:16:43 -0700

    flexbox and templates

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # flexbox-and-templating
+
+Number and name of feature: Feature 1: Pagination
+
+Estimate of time needed to complete: 1h
+
+Start time: 9.20 AM
+
+Finish time: 10 AM
+
+Actual time needed to complete: 40 min
+
+
+Number and name of feature: Feature 2: Templating
+
+Estimate of time needed to complete: 1h
+
+Start time: 10 AM
+
+Finish time: 11.10 AM
+
+Actual time needed to complete: 1.10 h
+
+
+Number and name of feature: Feature 3: Styling with Flexbox
+
+Estimate of time needed to complete: 30 min
+
+Start time: 11.20 AM 
+
+Finish time: 11.40 AM
+
+Actual time needed to complete: 20 min
+
+
+Number and name of feature: Feature 4: Sort the images
+
+Estimate of time needed to complete: 1h
+
+Start time: 11.50 AM 
+
+Finish time: 12.50 PM 
+
+Actual time needed to complete: 1h
---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict';
+
+let page1 = '/data/page-1.json';
+let page2 = '/data/page-2.json';
+let currentPage = page1;
+
+let template = $('#images').html();
+
+const addValuesToSection = (item) => {
+  const renderedTemplate = Mustache.render(template, item);
+  $('#template').append(renderedTemplate);
+};
+
+const addValuesToDropdown = (keyword) => {
+  $('#dropdown-menu').append(`
+      <option>${keyword}</option>
+    `);
+};
+
+$('#dropdown-menu').change(function() {
+  var selectedImage = $(this).children('option:selected').val();
+  $('.image').hide();
+  $('.' + selectedImage).show();
+});
+
+$('#sort-menu').change(function() {
+  var selectedSorting = $(this).children('option:selected').val();
+  let objectsArray = [];
+
+  $.ajax(currentPage).then(data => {
+    data.forEach(element => {
+      objectsArray.push(element);
+  })
+
+  if(selectedSorting === 'title') {
+    objectsArray.sort(function(a, b){
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    });
+  }
+
+  if (selectedSorting === 'number of horns') {
+    objectsArray.sort(function(a, b){
+      if (a.horns < b.horns) return -1;
+      if (a.horns > b.horns) return 1;
+    });
+  }
+    
+  renderData(objectsArray);
+  });
+});
+
+$('#next').click(function() {
+  currentPage = page2;
+  renderFile(currentPage);
+  addDropdownValues(currentPage);
+  $('#prev').show();
+  $('#next').hide();
+});
+
+$('#prev').click(function() {
+  currentPage = page1;
+  renderFile(currentPage);
+  addDropdownValues(currentPage);
+  $('#next').show();
+  $('#prev').hide();
+});
+
+function renderFile(jsonFile) {
+  $.ajax(jsonFile).then(data => {
+    renderData(data);
+  });
+}
+
+function renderData(data) {
+  $("#template").empty();
+  data.forEach(addValuesToSection);
+}
+
+function addDropdownValues(jsonFile) {
+  $("#dropdown-menu").empty();
+  $.ajax(jsonFile).then(data => {
+    let keywordArray = [];
+    data.forEach(element => {
+      if(!keywordArray.includes(element.keyword)) {
+        keywordArray.push(element.keyword);
+      }
+    });
+    keywordArray.forEach(addValuesToDropdown);
+  });
+}
+
+renderFile(currentPage);
+addDropdownValues(currentPage);
